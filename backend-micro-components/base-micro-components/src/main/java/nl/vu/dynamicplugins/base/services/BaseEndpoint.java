812d6328726323aab92f8759368e5ce10cdbec15
Corneliu-Dumitru Soficu <corneliudumitru.sofi@fireeye.com>
2021-09-24 16:07:39 +0300

    Added mc-cli project to simplify the creation of boilerplate code

--- backend-micro-components/base-micro-components/src/main/java/nl/vu/dynamicplugins/base/services/BaseEndpoint.java
+++ backend-micro-components/base-micro-components/src/main/java/nl/vu/dynamicplugins/base/services/BaseEndpoint.java
@@ -1,31 +1,28 @@
 package nl.vu.dynamicplugins.base.services;
 
 import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
-import nl.vu.dynamicplugins.base.utils.MicroComponentUtils;
 import org.apache.cxf.dosgi.common.api.IntentsProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 import java.io.InputStream;
+import java.net.URL;
 import java.util.Collections;
 import java.util.List;
 
 public class BaseEndpoint implements IntentsProvider {
-
-    protected MicroComponentUtils componentUtils;
-
-    public BaseEndpoint() {
-        componentUtils = new MicroComponentUtils();
-    }
+    Logger logger = LoggerFactory.getLogger(BaseEndpoint.class);
 
     @GET
     @Produces(value = "text/javascript")
     @Path("view")
     public Response view() {
-        InputStream inputStream = componentUtils.getViewFile(getClass().getClassLoader());
-        return componentUtils.buildInputStreamResponseResponse(inputStream);
+        InputStream inputStream = getViewFile(getClass().getClassLoader());
+        return buildInputStreamResponseResponse(inputStream);
     }
 
     @GET
@@ -40,5 +37,18 @@ public class BaseEndpoint implements IntentsProvider {
     @Override
     public List<?> getIntents() {
         return Collections.singletonList(new JacksonJaxbJsonProvider());
+    }
+
+    public InputStream getViewFile(ClassLoader classLoader) {
+        URL url = classLoader.getResource("view.js");
+        logger.info("Loading view.js file from location: {}", url);
+        return classLoader.getResourceAsStream("view.js");
+    }
+
+    public Response buildInputStreamResponseResponse(InputStream inputStream) {
+        Response.ResponseBuilder rb = Response
+                .ok(inputStream)
+                .header("Access-Control-Allow-Origin", "*");
+        return rb.build();
     }
 }
---
+++ mc-cli/backend_generator.py
@@ -0,0 +1,120 @@
+from jinja2 import Template
+import pathlib
+import os
+import sys
+
+
+class BackendGenerator:
+    GROUP_ID = "nl.vu.dynamicplugins"
+
+    def __init__(self, name, description):
+        self._name = name
+        self._description = description
+        self._current_directory = pathlib.Path(__file__).parent.resolve()
+        self._location_of_component_directory = f"{self._current_directory}/{self._name}"
+        self._artifact_id = self._name.lower()
+        self._bundle_name = f"{BackendGenerator.GROUP_ID}.{self._artifact_id}"
+        self._description = self._description
+        self._main_package_name = self._artifact_id.replace("-", "")
+
+    def generate(self):
+        self._create_folder_for_component()
+        self._generate_pom_xml_file()
+        self._generate_file_structure_for_java_project()
+
+    def _create_folder_for_component(self):
+        if os.path.exists(self._location_of_component_directory):
+            print(f"Directory for component already exists: {self._location_of_component_directory}\nAborting!")
+            sys.exit(-1)
+
+        print(f"Creating component directory: {self._location_of_component_directory}")
+        os.mkdir(self._location_of_component_directory)
+
+    def _generate_pom_xml_file(self):
+
+        with open('templates/pom.xml.jinja') as file_:
+            pom_xml_template = Template(file_.read())
+
+            activator_location = f"{BackendGenerator.GROUP_ID}.{self._main_package_name}.Activator"
+            export_package = f"{BackendGenerator.GROUP_ID}.{self._main_package_name}.*"
+
+            generated_pom_xml = pom_xml_template.render(
+                group_id=BackendGenerator.GROUP_ID,
+                artifact_id=self._artifact_id,
+                bundle_name=self._bundle_name,
+                description=self._description,
+                activator_location=activator_location,
+                export_package=export_package
+            )
+
+        with open(f"{self._location_of_component_directory}/pom.xml", "w") as f:
+            f.write(generated_pom_xml)
+
+        print("Created pom.xml file in the component directory")
+
+    def _generate_file_structure_for_java_project(self):
+        group_id_dirs = BackendGenerator.GROUP_ID.split('.')
+        file_structure_java_project = 'src/main/java/' + '/'.join(group_id_dirs) + '/' + self._main_package_name
+        file_structure_resources = 'src/main/resources'
+        file_structure_test = 'src/test/java'
+
+        entire_project_structure = self._location_of_component_directory + '/' + file_structure_java_project
+        entire_services_structure = entire_project_structure + '/services'
+        entire_resources_structure = self._location_of_component_directory + '/' + file_structure_resources
+        entire_test_structure = self._location_of_component_directory + '/' + file_structure_test
+
+        # Generating File Structure for the created micro-component
+        os.makedirs(entire_project_structure)
+        os.mkdir(entire_services_structure)
+        os.mkdir(entire_resources_structure)
+        os.makedirs(entire_test_structure)
+
+        print("Generated entire file structure for micro-component")
+
+        class_name_components = [c.capitalize() for c in self._name.split("-")]
+        class_name = "".join(class_name_components) + "Service"
+        endpoint_name = "-".join(c.lower() for c in self._name.split("-"))
+
+        # Generating the main Service Java Class
+        with open('templates/Service.java.jinja') as file_:
+            service_java = Template(file_.read())
+            generated_service_java = service_java.render(
+                class_name=class_name,
+                endpoint_name=endpoint_name
+            )
+
+        with open(f"{entire_services_structure}/{class_name}.java", "w") as f:
+            f.write(generated_service_java)
+
+        print(f"Generated {class_name}.java file")
+
+        # Generating the Activator Java Class
+        with open('templates/Activator.java.jinja') as file_:
+            activator_java = Template(file_.read())
+            generated_activator_java = activator_java.render(
+                endpoint_name=endpoint_name
+            )
+
+        with open(f"{entire_project_structure}/Activator.java", "w") as f:
+            f.write(generated_activator_java)
+
+        # Generating the default view.js script file
+        with open('templates/view.js.jinja') as file_:
+            view_js = Template(file_.read())
+            generated_view_js = view_js.render(
+                endpoint_name=endpoint_name
+            )
+
+        with open(f"{entire_resources_structure}/view.js", "w") as f:
+            f.write(generated_view_js)
+
+
+
+
+
+
+
+
+
+
+
