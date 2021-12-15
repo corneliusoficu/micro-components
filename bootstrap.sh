104cf1430bf87bbf645e120823b417efd503458c
Corneliu-Dumitru Soficu <corneliudumitru.sofi@mandiant.com>
2021-12-15 02:12:51 +0200

    Updated Vagrant configuration to correctly store the karaf public/private keys

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,9 @@
 ANGULAR_CLI_VERSION=9.0.3
 APACHE_KARAF_VERSION=4.3.3
 
+echo "Executing apt-get update"
+sudo apt-get update
+
 echo "Executing provisioning script for micro-components"
 
 install_pkg_if_not_present () {
@@ -42,7 +45,7 @@ if which node > /dev/null
     else
         echo "Installing npm"
         curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-        sudo apt install nodejs
+        sudo apt-get --yes install nodejs
         echo "node version: : $(node --version)"
         echo "npm version: $(npm --version)"
 fi
@@ -69,24 +72,33 @@ if [ ! -d "/opt/apache-karaf-${APACHE_KARAF_VERSION}" ]; then
     tar -xzf apache-karaf-"${APACHE_KARAF_VERSION}".tar.gz 
     rm -f apache-karaf-"${APACHE_KARAF_VERSION}".tar.gz
     chown vagrant:vagrant -R /opt/apache-karaf-"${APACHE_KARAF_VERSION}"/
+    
     cd /home/vagrant/.ssh
     ssh-keygen -t dsa -f karaf.id_dsa -N karaf
-    PUBLIC_KEY_CONTENTS="karaf=$(cat /home/vagrant/.ssh/karaf.id_dsa.pub)"
+    PUBLIC_KEY_CONTENTS="$(cat /home/vagrant/.ssh/karaf.id_dsa.pub)"
+    REGEX_PUB_KEY="ssh-dss\s+(.*)\s+root@ubuntu-bionic"
+    [[ $PUBLIC_KEY_CONTENTS =~ $REGEX_PUB_KEY ]]
+    PUBLIC_KEY_CONTENTS="${BASH_REMATCH[1]}"
+    echo "karaf=${PUBLIC_KEY_CONTENTS},_g_:admingroup" > /opt/apache-karaf-"${APACHE_KARAF_VERSION}"/etc/keys.properties
+    echo "_g_\:admingroup = group,admin,manager,viewer,systembundles,ssh" >> /opt/apache-karaf-"${APACHE_KARAF_VERSION}"/etc/keys.properties
+    
+    echo "karaf = karaf,_g_:admingroup" > /opt/apache-karaf-"${APACHE_KARAF_VERSION}"/etc/users.properties
+    echo "_g_\:admingroup = group,admin,manager,viewer,systembundles,ssh" >> /opt/apache-karaf-"${APACHE_KARAF_VERSION}"/etc/users.properties
 fi
 
 # MC-CLI DEPENDENCIES
 
-if [ ! -d "/vagrant/mc-cli/venv" ]; then
-    echo "Installing dependencies for mc-cli"
-    pip3 install -r /vagrant/mc-cli/requirements.txt
-    ln -s /vagrant/mc-cli/mc-cli /bin/mc-cli 
-fi
+echo "Installing dependencies for mc-cli"
+pip3 install -r /vagrant/mc-cli/requirements.txt
+ln -s /vagrant/mc-cli/mc-cli /bin/mc-cli 
+
 
 # ENVIRONMENT VARIABLES
 
 echo "export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64" >> /home/vagrant/.bashrc
 echo "export PATH=$PATH:$JAVA_HOME/bin" >> /home/vagrant/.bashrc
 echo "export KARAF_HOME=/opt/apache-karaf-${APACHE_KARAF_VERSION}" >> /home/vagrant/.bashrc
+echo "export KARAF_USER_PRIVATE_KEY_LOCATION=/home/vagrant/.ssh/karaf.id_dsa" >> /home/vagrant/.bashrc
 
 
 
--- mc-cli/constants.py
+++ mc-cli/constants.py
@@ -12,7 +12,7 @@ CURRENT_WORKING_DIR = os.getcwd()
 # TODO: Change this to point to correct location or read from PATH
 KARAF_BIN_LOCATION = f"{os.getenv('KARAF_HOME')}/bin"
 # TODO: Change this to point to correct location or read from PATH
-KARAF_USER_PRIVATE_KEY_LOCATION = f"{MC_CLI_HOME_PATH}/karaf.id_rsa"
+KARAF_USER_PRIVATE_KEY_LOCATION = f"{os.getenv('KARAF_USER_PRIVATE_KEY_LOCATION')}"
 
 ANGULAR_REGEX_CLI_VERSION = re.compile(r"Angular CLI: *((\d+)\.?(\d+)?\.?(\d+)?)")
 ANGULAR_DEFAULT_STYLE = "css"
